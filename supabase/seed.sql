7b5ff058950cc6deb807cd310a7ea32763d24198
Arnelle Balane <arnellebalane@gmail.com>
2023-10-07 18:46:26 -0700

    Add seed data for transaction_types and categories table

---
+++ supabase/seed.sql
@@ -0,0 +1,12 @@
+-- seed transaction_types table
+insert into transaction_types (name) values
+    ('Balance'),
+    ('Expense');
+
+-- seed categories table
+insert into categories (name) values
+    ('Food'),
+    ('Transportation'),
+    ('Rent'),
+    ('Groceries'),
+    ('Bills');
