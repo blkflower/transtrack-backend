1e2651deccef23fcedf3d9a040f360c13da984bd
philip.calape <philip.calape@whitecloak.com>
2023-10-13 00:30:30 +0800

    Modify wrong guard used

--- src/modules/transaction/adapter/controller/transaction.controller.ts
+++ src/modules/transaction/adapter/controller/transaction.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { TransactionService } from '../../usecase/service/transaction.service';
-import { AuthGuard } from '@nestjs/passport';
+import { SupabaseGuard } from 'src/modules/auth/guard/auth.guard';
 
 @Controller()
 export class TransactionController {
     constructor(private readonly transactionService: TransactionService) {}
 
     @Get()
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(SupabaseGuard)
     getHello(): string {
         return this.transactionService.getHello();
     }
