db70590d0a58c23c99cfd205acddc67842557ef4
philip.calape <philip.calape@whitecloak.com>
2023-10-19 14:32:41 +0800

    modify exporting of env variables

--- src/modules/common/environment/index.ts
+++ src/modules/common/environment/index.ts
@@ -2,9 +2,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL || '';
-const SUPABASE_KEY = process.env.SUPABASE_KEY || '';
-const SUPABASE_JWT_SECRET = process.env.SUPABASE_JWT_SECRET || '';
-const ENABLE_CATEGORY_MUTATION = process.env.ENABLE_CATEGORY_MUTATION === 'true';
-
-export { SUPABASE_URL, SUPABASE_KEY, SUPABASE_JWT_SECRET, ENABLE_CATEGORY_MUTATION };
+export const SUPABASE_URL = process.env.SUPABASE_URL || '';
+export const SUPABASE_KEY = process.env.SUPABASE_KEY || '';
+export const SUPABASE_JWT_SECRET = process.env.SUPABASE_JWT_SECRET || '';
+export const ENABLE_CATEGORY_MUTATION = process.env.ENABLE_CATEGORY_MUTATION === 'true';
